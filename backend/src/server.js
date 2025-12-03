7658243097143cdb1822ca0ec839b39d75197579
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-12-03 12:41:19 +0530

    database setup completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,34 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import  path from 'path';
-import {ENV} from './lib/env.js';
-
+import path from "path";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const __dirname = path.resolve();
 
-app.get('/health', (req, res) => { 
-    res.status(200).send('Server is healthy');
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy");
 });
-app.get('/book', (req, res) => { 
-    res.status(200).send('Server is booking');
+app.get("/book", (req, res) => {
+  res.status(200).send("Server is booking");
 });
-
 
 // make our app ready for deployment
- if(ENV.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend","dist","index.html"));
-    });
- }
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.listen( ENV.PORT, () => {
-    console.log(`Server is running on  port ${ENV.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on  port ${ENV.PORT}`);});
+  } catch (error) {
+    console.error(`Failed to start server: ${error.message}`);
+  }
+};
+
+startServer();
